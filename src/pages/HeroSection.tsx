dfa56956e951af29ff10bdbda39d33d44617ed00
Lacrizomiq <118858104+Lacrizomiq@users.noreply.github.com>
2024-04-15 21:13:31 +0200

    Update HeroSection.tsx

--- src/pages/HeroSection.tsx
+++ src/pages/HeroSection.tsx
@@ -4,13 +4,13 @@ import { Button } from "@/components/ui/button";
 export const HeroSection = () => (
   <div className="w-full py-8 lg:py-8">
     <div className="container mx-auto">
-      <div className="grid grid-cols-1 gap-8 items-center md:grid-cols-2">
-        <div className="flex gap-4 flex-col">
-          <div className="flex gap-4 flex-col">
-            <h1 className="text-5xl md:text-7xl max-w-lg tracking-tighter text-left font-regular">
+      <div className="grid items-center grid-cols-1 gap-8 md:grid-cols-2">
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4">
+            <h1 className="max-w-lg text-5xl tracking-tighter text-left md:text-7xl font-regular">
               C'est ici que commence ta transformation physique
             </h1>
-            <p className="text-xl leading-relaxed tracking-tight text-muted-foreground max-w-md text-left">
+            <p className="max-w-md text-xl leading-relaxed tracking-tight text-left text-muted-foreground">
               Masseurs professionnels et Coachs sportifs, nous t'accompagnons
               dans tes projets afin que tu puisses libérer le plein potentiel de
               ton corps.
@@ -27,9 +27,9 @@ export const HeroSection = () => (
           </div>
         </div>
         <div className="grid grid-cols-2 gap-8">
-          <div className="bg-muted rounded-md aspect-square"></div>
-          <div className="bg-muted rounded-md row-span-2"></div>
-          <div className="bg-muted rounded-md aspect-square"></div>
+          <div className="rounded-md bg-muted aspect-square"></div>
+          <div className="row-span-2 rounded-md bg-muted"></div>
+          <div className="rounded-md bg-muted aspect-square"></div>
         </div>
       </div>
     </div>
