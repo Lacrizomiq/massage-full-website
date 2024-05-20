5afb49fc8cf17e80a34cf3b716e72305b159ebf2
Lacrizomiq <118858104+Lacrizomiq@users.noreply.github.com>
2024-05-20 11:09:42 +0200

    removing console.log

--- src/utils/PhoneCallButton.tsx
+++ src/utils/PhoneCallButton.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import { PhoneCall } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { useEffect } from "react";
 
 const PhoneCallButton: React.FC = () => {
   const phoneNumbers = [
     import.meta.env.VITE_PHONE_NUMBER_1 || "",
     import.meta.env.VITE_PHONE_NUMBER_2 || "",
   ];
-
-  useEffect(() => {
-    // Vérifiez que les numéros de téléphone sont bien définis
-    console.log("VITE_PHONE_NUMBER_1:", import.meta.env.VITE_PHONE_NUMBER_1);
-    console.log("VITE_PHONE_NUMBER_2:", import.meta.env.VITE_PHONE_NUMBER_2);
-    console.log("Phone numbers:", phoneNumbers);
-  }, [phoneNumbers]);
 
   const getRandomPhoneNumber = () => {
     const randomIndex = Math.floor(Math.random() * phoneNumbers.length);
@@ -24,7 +16,6 @@ const PhoneCallButton: React.FC = () => {
   const handleButtonClick = () => {
     const phoneNumber = getRandomPhoneNumber();
     if (phoneNumber) {
-      console.log("Calling phone number:", phoneNumber);
       window.location.href = `tel:${phoneNumber}`;
     } else {
       console.error("Phone number is not defined");
